438c5b77e5129c62f7bf5ef3614f0af50e8c1073
SamuPasquier <samutr282003@gmail.com>
2025-02-27 08:40:01 -0400

    quinto commit

--- manage.py
+++ manage.py
@@ -2,11 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import django
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'page_permisos.settings')
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,6 +16,25 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    # Inicializar Django antes de ejecutar cualquier consulta a la base de datos
+    django.setup()
+
+    # Crear superusuario si no existe
+    from django.contrib.auth import get_user_model
+
+    SUPERUSER_USERNAME = "fdiaz"
+    SUPERUSER_EMAIL = "pepe@gmail.com"
+    SUPERUSER_PASSWORD = "C1D1M3C.imt"
+
+    User = get_user_model()
+    
+    if not User.objects.filter(username=SUPERUSER_USERNAME).exists():
+        User.objects.create_superuser(SUPERUSER_USERNAME, SUPERUSER_EMAIL, SUPERUSER_PASSWORD)
+        print("✅ Superusuario creado correctamente.")
+    else:
+        print("⚠️ El superusuario ya existe.")
+
     execute_from_command_line(sys.argv)
 
 
